ca6d1c24f4f23c3a26866f6b8ef94b08a5021ee7
Elizabeth Tom <elizabeth_tom@excite.com>
2017-11-16 20:59:50 -0600

    Edited server.js to use controller.js, and updated controller.js

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,5 +1,41 @@
 var db = require("../models");
 
+var express = require("express");
+
+var router = express.Router();
+
+// Create our burger routes and set up logic within those routes where required.
+router.post("/api/leaders", function(req, res) {
+
+	db.leader.create({
+		leader_name: req.body.leader_name
+	}).then( function(response) {
+		res.redirect("/");
+	}).catch( function(error) {
+		res.json(error);
+	});
+
+});
+
+router.put("/api/update", function(req, res) {
+	
+	db.leader.update({
+		// devoured: true
+		// UPDATE TO RELEVANT INFO
+	}, {
+		where: {
+			id: req.body.leader_id
+		}
+	}).then( function(response) {
+		res.redirect("/");
+	}).catch( function(error) {
+		res.json(error);
+	});
+
+});
+
+module.exports = router;
+
 module.exports = function (app) {
 
 };
