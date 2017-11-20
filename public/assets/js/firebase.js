f3896c84bd3dee95e1911baa6e7e19b6706a5056
Elizabeth Tom <elizabeth_tom@excite.com>
2017-11-20 11:17:59 -0600

    Adding firebase code for anonymous login

--- public/assets/js/firebase.js
+++ public/assets/js/firebase.js
@@ -1,13 +1,28 @@
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyDwsIqLQ2130R93_l1lGndlduTtp69OMpU",
-  authDomain: "cards-against-extraterrestials.firebaseapp.com",
-  databaseURL: "https://cards-against-extraterrestials.firebaseio.com",
-  projectId: "cards-against-extraterrestials",
-  storageBucket: "cards-against-extraterrestials.appspot.com",
-  messagingSenderId: "1038459487422"
-};
-firebase.initializeApp(config);
+        apiKey: "AIzaSyDwsIqLQ2130R93_l1lGndlduTtp69OMpU",
+        authDomain: "cards-against-extraterrestials.firebaseapp.com",
+        databaseURL: "https://cards-against-extraterrestials.firebaseio.com",
+        projectId: "cards-against-extraterrestials",
+        storageBucket: "cards-against-extraterrestials.appspot.com",
+        messagingSenderId: "1038459487422"
+    };
+    firebase.initializeApp(config);
 
-var database = firebase.database();
+    var players = firebase.database().ref('/players')
+
+   const btnLogin = $("#login");
+
+   console.log(btnLogin);
+   
+   btnLogin.on("click",function(){
+    firebase.auth().signInAnonymously();
+   });
+
+   firebase.auth().onAuthStateChanged(firebaseUser=>{
+    console.log(firebaseUser)
+    console.log(firebaseUser.uid)
+    players.push(firebaseUser.uid)
+
+   });
 
