90c4b6281039477e733b6a85b0d90268ce8b092e
Elizabeth Tom <elizabeth_tom@excite.com>
2017-11-30 13:45:20 -0600

    Move player's response to  response row and added computer players

--- public/assets/js/firebase.js
+++ public/assets/js/firebase.js
@@ -1,5 +1,6 @@
-// Initialize Firebase
-var config = {
+$(document).ready(function () {
+    // Initialize Firebase
+    var config = {
         apiKey: "AIzaSyDwsIqLQ2130R93_l1lGndlduTtp69OMpU",
         authDomain: "cards-against-extraterrestials.firebaseapp.com",
         databaseURL: "https://cards-against-extraterrestials.firebaseio.com",
@@ -8,36 +9,28 @@ var config = {
         messagingSenderId: "1038459487422"
     };
     firebase.initializeApp(config);
-
-    var players = firebase.database().ref('/players')
-
-   const btnLogin = $("#login");
-
-   console.log(btnLogin);
-   
-   btnLogin.on("click",function(){
-    firebase.auth().signInAnonymously();
-   });
-
-   firebase.auth().onAuthStateChanged(firebaseUser=>{
-    console.log(firebaseUser)
-    console.log(firebaseUser.uid)
-    players.push(firebaseUser.uid)
-
-   });
-
-var players = database.ref('/players');
-
-const btnLogin = $("#login");
-
-console.log(btnLogin);
-
-btnLogin.on("click",function(){
-    firebase.auth().signInAnonymously();
-});
-
-firebase.auth().onAuthStateChanged(firebaseUser=>{
-	console.log(firebaseUser);
-	console.log(firebaseUser.uid);
-	players.push(firebaseUser.uid);
+    var database = firebase.database();
+    var userName = "";
+    var playersRef = database.ref("players");
+    $("#login").click(function (event) {
+        //sets player's username based on their input
+        userName = $("#userName").val().trim();
+        // Creates key based on assigned player number
+        playersRef = database.ref("/players/" + userName);
+        //updates firebase when a player joins
+        playersRef.set({
+            gameJoined: "yes"
+        });
+    });
+    playersRef.on("value", function (snapshot) {
+        console.log(snapshot.numChildren());
+        if (snapshot.numChildren() === 5) {
+            console.log("game starting");
+        }
+        var playerOneExists = snapshot.child().exists();
+        var playerOneData = snapshot.child("1").val();
+        console.log(playerOneExists);
+        console.log(playerOneData);
+    });
+    playersRef.onDisconnect().remove();
 });
